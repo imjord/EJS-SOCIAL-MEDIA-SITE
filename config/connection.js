359966a3192308d7d292acfd57e27575e58ea492
imjord <dean.jay.goat@gmail.com>
2022-04-21 22:13:13 -0600

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost/robustbackend');
+mongoose.connect('mongodb+srv://imjord:imjord123@cluster0.da5bc.mongodb.net/simple-posts?retryWrites=true&w=majority');
 
 
 
