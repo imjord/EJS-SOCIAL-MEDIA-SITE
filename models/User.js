7afe93ed7fceec1e0173be2ab3a1b3ffe0baa975
imjord <dean.jay.goat@gmail.com>
2022-04-24 21:15:44 -0600

    added user model

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const {Schema, model} = require('mongoose');
+
+
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const User = new model('User', UserSchema);
+
+module.exports = User;
