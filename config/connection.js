b5d118d828c65aa2089b8280b7bd15b327c7f443
imjord <dean.jay.goat@gmail.com>
2023-01-18 20:18:38 -0700

    trying to deploy to heroku

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 
-mongoose.connect("mongodb+srv://imjord:imjord123@cluster0.mkbgsyb.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 
 
