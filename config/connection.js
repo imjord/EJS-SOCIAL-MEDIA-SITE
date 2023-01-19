fd264c454e24c1e988175fb0faafc489cb7411c4
imjord <dean.jay.goat@gmail.com>
2023-01-18 20:21:33 -0700

    heroku update

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect("mongodb+srv://imjord:imjord123@cluster0.mkbgsyb.mongodb.net/?retryWrites=true&w=majority");
 
 
 
