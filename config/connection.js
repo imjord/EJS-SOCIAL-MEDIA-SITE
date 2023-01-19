6980d8968b04499eefd3289726979e9df402b872
imjord <dean.jay.goat@gmail.com>
2023-01-18 20:03:07 -0700

    heroku

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ejssms");
+mongoose.connect("mongodb+srv://imjord:imjord123@cluster0.mkbgsyb.mongodb.net/?retryWrites=true&w=majority");
 
 
 
